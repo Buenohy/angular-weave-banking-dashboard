7655591c92a3aba725626e706ab09657531cd513
Gabriel Bueno Hygino <gabriel.buenohyginoc@gmail.com>
2026-01-27 17:16:17 -0300

    feat: icons nav-bar

--- src/app/components/nav-bar/nav-bar.ts
+++ src/app/components/nav-bar/nav-bar.ts
@@ -1,13 +1,37 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { NgIcon, provideIcons } from '@ng-icons/core';
-import { matAutoAwesomeMosaicOutline } from '@ng-icons/material-icons/outline';
+import {
+  matAutoAwesomeMosaicOutline,
+  matAnalyticsOutline,
+  matInsertDriveFileOutline,
+  matPeopleOutline,
+  matCloudOutline,
+  matCreditCardOutline,
+  matReceiptOutline,
+  matWalletOutline,
+  matCardTravelOutline,
+  matFolderOutline,
+} from '@ng-icons/material-icons/outline';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
   imports: [NgIcon, RouterLink, RouterLinkActive],
-  viewProviders: [provideIcons({ matAutoAwesomeMosaicOutline })],
+  viewProviders: [
+    provideIcons({
+      matAutoAwesomeMosaicOutline,
+      matAnalyticsOutline,
+      matInsertDriveFileOutline,
+      matPeopleOutline,
+      matCloudOutline,
+      matCreditCardOutline,
+      matReceiptOutline,
+      matWalletOutline,
+      matCardTravelOutline,
+      matFolderOutline,
+    }),
+  ],
   templateUrl: './nav-bar.html',
   styleUrl: './nav-bar.css',
 })
@@ -15,47 +39,47 @@ export class navBar {
   navBarItems = [
     {
       label: 'Dashboard',
-      icon: 'matAutoAwesomeMosaicOutline',
+      icon: 'matFolderOutline',
       route: '/dashboard',
     },
     {
       label: 'Request',
-      icon: 'matAutoAwesomeMosaicOutline',
+      icon: 'matCardTravelOutline',
       route: '/request',
     },
     {
       label: 'Balance',
-      icon: 'matAutoAwesomeMosaicOutline',
+      icon: 'matWalletOutline',
       route: '/balance',
     },
     {
       label: 'Transaction',
-      icon: 'matAutoAwesomeMosaicOutline',
+      icon: 'matReceiptOutline',
       route: '/transaction',
     },
     {
       label: 'Card',
-      icon: 'matAutoAwesomeMosaicOutline',
+      icon: 'matCreditCardOutline',
       route: '/card',
     },
     {
       label: 'Subscription',
-      icon: 'matAutoAwesomeMosaicOutline',
+      icon: 'matCloudOutline',
       route: '/subscription',
     },
     {
       label: 'Employees',
-      icon: 'matAutoAwesomeMosaicOutline',
+      icon: 'matPeopleOutline',
       route: '/employees',
     },
     {
       label: 'Documents',
-      icon: 'matAutoAwesomeMosaicOutline',
+      icon: 'matInsertDriveFileOutline',
       route: '/documents',
     },
     {
       label: 'Analytics',
-      icon: 'matAutoAwesomeMosaicOutline',
+      icon: 'matAnalyticsOutline',
       route: '/analytics',
     },
   ];
