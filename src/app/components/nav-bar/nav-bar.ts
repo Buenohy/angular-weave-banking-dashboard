aa5363a6d8c54271ddca165c3519aa4b4c0777a9
Gabriel Bueno Hygino <gabriel.buenohyginoc@gmail.com>
2026-01-27 16:13:53 -0300

    feat: Ui and instal material icons google

--- src/app/components/nav-bar/nav-bar.ts
+++ src/app/components/nav-bar/nav-bar.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { matAutoAwesomeMosaicOutline } from '@ng-icons/material-icons/outline';
 
 @Component({
   selector: 'app-nav-bar',
-  imports: [],
+  standalone: true,
+  imports: [NgIcon],
+  viewProviders: [provideIcons({ matAutoAwesomeMosaicOutline })],
   templateUrl: './nav-bar.html',
   styleUrl: './nav-bar.css',
 })
-export class NavBar {
-
-}
+export class NavBar {}
