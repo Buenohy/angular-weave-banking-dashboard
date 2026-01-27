a592644916eb5c78f23fab92a835c766c47ce5c1
Gabriel Bueno Hygino <gabriel.buenohyginoc@gmail.com>
2026-01-27 16:51:37 -0300

    feat: using for to make a map navBarItems

--- src/app/components/nav-bar/nav-bar.ts
+++ src/app/components/nav-bar/nav-bar.ts
@@ -1,13 +1,62 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 import { NgIcon, provideIcons } from '@ng-icons/core';
 import { matAutoAwesomeMosaicOutline } from '@ng-icons/material-icons/outline';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [NgIcon],
+  imports: [NgIcon, RouterLink, RouterLinkActive],
   viewProviders: [provideIcons({ matAutoAwesomeMosaicOutline })],
   templateUrl: './nav-bar.html',
   styleUrl: './nav-bar.css',
 })
-export class NavBar {}
+export class navBar {
+  navBarItems = [
+    {
+      label: 'Dashboard',
+      icon: 'matAutoAwesomeMosaicOutline',
+      route: '/dashboard',
+    },
+    {
+      label: '',
+      icon: '',
+      route: '',
+    },
+    {
+      label: '',
+      icon: '',
+      route: '',
+    },
+    {
+      label: '',
+      icon: '',
+      route: '',
+    },
+    {
+      label: '',
+      icon: '',
+      route: '',
+    },
+    {
+      label: '',
+      icon: '',
+      route: '',
+    },
+    {
+      label: '',
+      icon: '',
+      route: '',
+    },
+    {
+      label: '',
+      icon: '',
+      route: '',
+    },
+    {
+      label: '',
+      icon: '',
+      route: '',
+    },
+  ];
+}
