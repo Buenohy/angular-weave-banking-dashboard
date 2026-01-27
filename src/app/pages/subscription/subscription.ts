fa2ebc0f202fe5dc4f2e8200ae45748245a88c35
Gabriel Bueno Hygino <gabriel.buenohyginoc@gmail.com>
2026-01-27 19:25:54 -0300

    feat: table component

--- src/app/pages/subscription/subscription.ts
+++ src/app/pages/subscription/subscription.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { ButtonAdd } from '../../components/button-add/button-add';
 import { Input } from '../../components/input/input';
 import { Button } from '../../components/button/button';
+import { Table } from '../../components/table/table';
 
 @Component({
   selector: 'app-subscription',
-  imports: [ButtonAdd, Input, Button],
+  imports: [ButtonAdd, Input, Button, Table],
   templateUrl: './subscription.html',
   styleUrl: './subscription.css',
 })
