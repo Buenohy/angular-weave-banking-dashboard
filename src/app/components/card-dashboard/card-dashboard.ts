ce5fcee1a4685a48ffeaec2cf36fcd1f739f7c46
Gabriel Bueno Hygino <gabriel.buenohyginoc@gmail.com>
2026-01-29 18:58:55 -0300

    feat: card-dashboard Ui without icons

---
+++ src/app/components/card-dashboard/card-dashboard.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-dashboard',
+  standalone: true,
+  imports: [],
+  templateUrl: './card-dashboard.html',
+  styleUrl: './card-dashboard.css',
+})
+export class CardDashboard {}
--- src/app/pages/dashboard/dashboard.ts
+++ src/app/pages/dashboard/dashboard.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { CardDashboard } from '../../components/card-dashboard/card-dashboard';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [CardDashboard],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css',
 })
-export class Dashboard {
-
-}
+export class Dashboard {}
