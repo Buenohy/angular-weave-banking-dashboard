a68e4d08e57d3c4390986ecc77f3c10f0df58446
Gabriel Bueno Hygino <gabriel.buenohyginoc@gmail.com>
2026-01-29 16:37:10 -0300

    feat: added input icons for button-add component

--- src/app/components/button-add/button-add.ts
+++ src/app/components/button-add/button-add.ts
@@ -1,17 +1,14 @@
-import { Component } from '@angular/core';
-import { NgIcon, provideIcons } from '@ng-icons/core';
-import { matPlusOutline } from '@ng-icons/material-icons/outline';
+import { Component, input } from '@angular/core';
+import { NgIcon } from '@ng-icons/core';
 
 @Component({
   selector: 'app-button-add',
   standalone: true,
   imports: [NgIcon],
-  viewProviders: [
-    provideIcons({
-      matPlusOutline,
-    }),
-  ],
   templateUrl: './button-add.html',
   styleUrl: './button-add.css',
 })
-export class ButtonAdd {}
+export class ButtonAdd {
+  label = input.required<string>();
+  icon = input<string>();
+}
--- src/app/pages/subscription/subscription.ts
+++ src/app/pages/subscription/subscription.ts
@@ -3,10 +3,17 @@ import { ButtonAdd } from '../../components/button-add/button-add';
 import { Input } from '../../components/input/input';
 import { Button } from '../../components/button/button';
 import { Table } from '../../components/table/table';
+import { provideIcons } from '@ng-icons/core';
+import { matPlusOutline } from '@ng-icons/material-icons/outline';
 
 @Component({
   selector: 'app-subscription',
   imports: [ButtonAdd, Input, Button, Table],
+  viewProviders: [
+    provideIcons({
+      matPlusOutline,
+    }),
+  ],
   templateUrl: './subscription.html',
   styleUrl: './subscription.css',
 })
