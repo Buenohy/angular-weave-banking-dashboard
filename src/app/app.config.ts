1a6be1c204e27b69531282160baf61c7f78c2941
Gabriel Bueno Hygino <gabriel.buenohyginoc@gmail.com>
2026-01-30 13:09:30 -0300

    feat: config ngx-echarts in app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,9 +4,35 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
+import { provideEchartsCore } from 'ngx-echarts';
+import * as echarts from 'echarts/core';
+import { CanvasRenderer } from 'echarts/renderers';
+
+import { LineChart, BarChart, PieChart } from 'echarts/charts';
+
+import {
+  GridComponent,
+  TooltipComponent,
+  LegendComponent,
+  DataZoomComponent,
+} from 'echarts/components';
+
+echarts.use([
+  CanvasRenderer,
+  LineChart,
+  BarChart,
+  PieChart,
+  GridComponent,
+  TooltipComponent,
+  LegendComponent,
+  DataZoomComponent,
+]);
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
-  ]
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
+    provideEchartsCore({ echarts }),
+  ],
 };
